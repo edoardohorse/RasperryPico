304989dbfab59937e133d1f60c0bac0ab05833b5
Edoardo Cavallo <iweek16@gmail.com>
2024-09-12 14:19:28 +0200

    Better manage of press btn

--- micropython/button/button.py
+++ micropython/button/button.py
@@ -1,11 +1,10 @@
 import machine
-import utime
+import time
 
 # Imposta il pin del pulsante (adatta al tuo circuito)
 button_pin = 17
 
 led = machine.Pin("LED", machine.Pin.OUT)
-
 
 # Imposta il pin come ingresso con pull-down
 button = machine.Pin(button_pin, machine.Pin.IN, machine.Pin.PULL_DOWN)
@@ -13,13 +12,15 @@ button = machine.Pin(button_pin, machine.Pin.IN, machine.Pin.PULL_DOWN)
 # Opzionale: imposta il pin del LED come uscita
 # led = machine.Pin(led_pin, machine.Pin.OUT)
 
-while True:
-    # print(button.value())
-    if button.value() == 0:
-        # print("Pulsante premuto")
-        led.on()
-    else:
-        # print("Pulsante rilasciato")
-        led.off()
+# Function to handle button press down
+def button_pressed():
+    print("Button pressed!")
+    # Add your desired actions here
 
-    utime.sleep_ms(100)
+
+# Main loop
+while True:
+    if button.value() == 0:
+        button_pressed()
+        while button.value() == 0:
+            time.sleep(0.01)  # Debounce delay
